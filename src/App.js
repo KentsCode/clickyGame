a7a5ee7709eeb5fcaadd961f93fc8ae561efb423
KentsCode <kentmbox42@yahoo.com>
2018-04-23 00:20:50 -0500

    shuffle working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
 import animals from './animals.json';
 import NavBar from './components/NavBar';
 import Card from './components/Card';
 import Wrapper from './components/Wrapper';
-
 
 class App extends Component {
   state = {
@@ -20,19 +18,20 @@ class App extends Component {
     this.setState({animals: this.shuffle(this.state.animals)})
   }
   shuffle = animals => {
+    console.log("shuffle called");
     //call this function each time to sort the animals in a random order. (called a Fisher-Yates-Durstenfeld shuffle.)
-    for (var i = 0; i < animals.length; i++) {
-      var j = i + Math.floor(Math.random() * (animals.length - i));
-
-        var temp = animals[j];
-        animals[j] = animals[i];
-        animals[i] = temp;
+    for (let i = 0; i < animals.length; i++) {
+      let j = i + Math.floor(Math.random() * (animals.length - i));
+      //console.log("shuffling");
+      let temp = animals[j];
+      animals[j] = animals[i];
+      animals[i] = temp;
     }
     return animals; 
   }
   imageClick = currentId => {
-    console.log("picture clicked " + currentId);
-    console.log(this.state.userGuessed.length);
+    //console.log("picture clicked " + currentId);
+    //console.log(this.state.userGuessed.length);
     if (this.state.userGuessed.includes(currentId)) {
       console.log("Game Over")
       this.setState({
@@ -44,21 +43,18 @@ class App extends Component {
       console.log("no match");
       this.state.userGuessed.push(currentId);
     }
-  
-    
+    this.componentDidMount();
+    //this.shuffle(this.state.animals);
+    //console.log(this.state.animals);
   }
   //correct 
 
   //incorrect
 
-   
-
-
   render() {
     return (
       <div>
         <NavBar score={this.state.score} highScore={this.state.highScore} />
-      
         <Wrapper>
           {this.state.animals.map((animalMapped) => (
             <Card
@@ -71,8 +67,8 @@ class App extends Component {
         </Wrapper>
       </div>
       
-     )}
-
+    )
+  }
 }
 
 export default App;
